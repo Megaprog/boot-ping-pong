dbbf13c0e425e4f9d26064f4bb185bfe7b3a8851
Tomas Shestakov <shestakovte@gmail.com>
2015-03-15 17:38:04 +0300

    handling exceptions

--- server/src/main/java/pp/controller/HandlerController.java
+++ server/src/main/java/pp/controller/HandlerController.java
@@ -2,8 +2,11 @@ package pp.controller;
 
 import com.datastax.driver.core.querybuilder.QueryBuilder;
 import org.jmmo.sc.Cassandra;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,17 +15,19 @@ import pp.model.User;
 import pp.protocol.PingRequest;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.UUID;
 
 @RestController
 public class HandlerController {
+    private static final Logger log = LoggerFactory.getLogger(HandlerController.class);
 
     @Autowired
     Cassandra cassandra;
 
     @RequestMapping(value = "/handler", method = RequestMethod.POST)
     public String handler(@RequestBody PingRequest pingRequest, HttpServletResponse response) {
-        if (!pingRequest.getType().equals("ping") || pingRequest.getUserId() == null) {
+        if (!pingRequest.getType().equals(PingRequest.TYPE) || pingRequest.getUserId() == null) {
             response.setStatus(HttpStatus.NOT_FOUND.value());
             return "";
         }
@@ -42,4 +47,13 @@ public class HandlerController {
 
         return "PONG " + user.getPings();
     }
+
+
+    @ExceptionHandler(Exception.class)
+    public void defaultHandler(HttpServletResponse response, Exception e) throws IOException {
+        log.warn("Unexpected exception caught", e);
+
+        response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
 }
